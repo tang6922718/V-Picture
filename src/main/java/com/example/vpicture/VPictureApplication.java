faba5009fdf2c308c089957f1f80be143f94bc70
tanglizhong <tanglizhonrg@bonc.com.cn>
2018-09-17 21:42:17 +0800

    Initial commit

---
+++ src/main/java/com/example/vpicture/VPictureApplication.java
@@ -0,0 +1,12 @@
+package com.example.vpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VPictureApplication.class, args);
+    }
+}
